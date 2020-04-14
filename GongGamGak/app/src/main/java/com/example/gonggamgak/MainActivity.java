9d2ad2c6d0c4aecd9eeccc153f1eb7c6a089b0f3
Kim_TaeHyeong <37135317+KimTaeHyeong17@users.noreply.github.com>
2020-04-15 01:19:18 +0900

    project init

---
+++ GongGamGak/app/src/main/java/com/example/gonggamgak/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gonggamgak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
